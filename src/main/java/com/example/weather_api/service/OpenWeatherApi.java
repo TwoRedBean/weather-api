9c1bae6d99dcc4476953c9c9d547957e829bafc0
TwoRedBean <89733180+TwoRedBean@users.noreply.github.com>
2025-08-15 21:57:35 +0800

    parse weather description

--- src/main/java/com/example/weather_api/service/OpenWeatherApi.java
+++ src/main/java/com/example/weather_api/service/OpenWeatherApi.java
@@ -1,5 +1,7 @@
 package com.example.weather_api.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -13,9 +15,17 @@ public class OpenWeatherApi {
     private String baseUrl;
 
     private final RestTemplate restTemplate = new RestTemplate();
+    private final ObjectMapper mapper = new ObjectMapper();
 
     public String getWeather() {
         String url = String.format("%s?q=%s&appid=%s", baseUrl, "London,uk", apiKey);
-        return restTemplate.getForObject(url, String.class);
+
+        try {
+            String response = restTemplate.getForObject(url, String.class);
+            JsonNode root = mapper.readTree(response);
+            return root.get("weather").get(0).get("description").asText();
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to get weather from weather API: ", e);
+        }
     }
 }
